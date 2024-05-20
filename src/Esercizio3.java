d3afef3405dbfc82e04660cb4f20bca7afb49bde
ABBA3124 <davideabbadessa73@gmail.com>
2024-05-20 17:59:57 +0200

    fix

--- src/Esercizio3.java
+++ src/Esercizio3.java
@@ -2,7 +2,7 @@ public class Esercizio3 {
     public static void main(String[] args) {
         System.out.println("esercizio 3 !");
         System.out.println(perimetroRettangolo(2,3));
-        System.out.println(pariOdispari(0));
+        System.out.println(pariOdispari(12));
         System.out.println(areaTriangolo(3,4,5));
     }
     public static double perimetroRettangolo(double l1, double l2){
